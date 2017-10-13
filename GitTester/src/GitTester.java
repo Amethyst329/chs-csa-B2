a3a4dc2ccc06d712ce7edcdec4d17ea949c63891
Amethyst329 <jamethyst329@gmail.com>
2017-10-13 09:53:13 -0600

    My second attempt.
    Testing Git and the system.

--- GitTester/src/GitTester.java
+++ GitTester/src/GitTester.java
@@ -4,6 +4,7 @@ public class GitTester {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("ayy");
+		System.out.println("A line 2 demonstrate a change");
 	}
 
 }
