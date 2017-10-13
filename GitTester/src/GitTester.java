7808368dd1e69e2b675d5058d0c6b4bb823be13b
Amethyst329 <jamethyst329@gmail.com>
2017-10-13 10:13:02 -0600

    Fifth try at Git
    5 ze teachere

--- GitTester/src/GitTester.java
+++ GitTester/src/GitTester.java
@@ -7,6 +7,7 @@ public class GitTester {
 		System.out.println("A line 2 demonstrate a change");
 		System.out.println("A thrild line for za teachere");
 		System.out.println("A fourthe line for za teachere");
+		System.out.println("A fitzle line for za teachere");
 	}
 
 }
